5c84f1591f9915f1d9a219a7ddcba60eb8200aa1
Johannes Hubertz <johannes@hubertz.de>
2013-07-14 22:32:14 +0200

    setup long_description now read from README.txt

--- setup.py
+++ setup.py
@@ -4,26 +4,7 @@ from distutils.core import setup
 
 setup(name='iptables-optimizer',
     description='runtime iptables sorting by packet-counters',
-    long_description="""
-    iptables-optimizer
-    ==================
-
-    This little python script is run by a wrapper (shell), driven
-    by cron. The wrapper needs to be run as root because it calls
-    iptables-save and iptables-restore.
-
-    Goal is to have all chains sorted by decreasing packet-counters,
-    as each incoming packet is searched along the chains within one
-    single core during interrupt processing. From a statistical point
-    of view, the load decreases by having them sorted by the counters,
-    if you have a lot of unused but neccessary commands within the
-    chains. The sorting is done within the lists build from the
-    iptables-save. Afterwards printed to stdout gives you a chance to
-    inspect them before restoring to the kernel by iptables-restore.
-
-    And of course, some resticitions are respected, especially no
-    rules with different policies are sorted crossover.
-    """,
+    long_description=open('README.txt').read(),
     version='0.9.1',
     license='GNU General Public License version 3 (or later)',
     platforms='GNU/Linux 2.6.x, GNU/Linux 3.x',
